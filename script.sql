7c380530fe5965e49f52ed07b24739bde2f0a4ef
BogdanPolitic <48634138+BogdanPolitic@users.noreply.github.com>
2020-04-20 19:16:51 +0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,7 @@
+create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, phone_number TEXT);
+insert into users (name, phone_number) values ('john', '+1 3932 94 2392');
+insert into users (name, phone_number) values ('marge', '+2 w239423 2389');
+insert into users (name, phone_number) values ('maggie', '+6 23q84238238');
+insert into users (name, phone_number) values ('pula', '+2384238974238972357923597');
+insert into users (name, phone_number) values ('lisa', '+3q9843298482894331842');
+insert into users (name, phone_number) values ('bart', '2192149');
